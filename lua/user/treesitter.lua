9e19a9a61d83302601be13b76aebd555b2a3395a
Dartsac <idobson.gatech@gmail.com>
2022-11-23 17:58:36 -0500

    fixed completing and alse the jsx indent problem. Added sticky header.

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -15,7 +15,7 @@ configs.setup {
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = { "yaml", "css", "javascript", "html"} },
+  indent = { enable = true, disable = { "yaml", "css", "html"} },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
