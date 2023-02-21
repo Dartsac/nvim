d97531eda1fccf32301af0f49374f96e771cbf1d
Dartsac <idobson.gatech@gmail.com>
2023-02-21 12:20:50 -0500

    remove alpha plugin and references as I don't see a need for it anymore

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,4 +1,4 @@
-vim.cmd [[
+vim.cmd([[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
@@ -23,12 +23,7 @@ vim.cmd [[
     autocmd!
     autocmd VimResized * tabdo wincmd = 
   augroup end
-
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
-]]
+]])
 
 -- Autoformat
 -- augroup _lsp
