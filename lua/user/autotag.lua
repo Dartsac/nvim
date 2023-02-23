e9ad4f4c709e0e4086484ce0141e7094b7c10bd6
Dartsac <idobson.gatech@gmail.com>
2023-02-23 17:16:22 -0500

    formatting

--- lua/user/autotag.lua
+++ lua/user/autotag.lua
@@ -1,6 +1,6 @@
-local status, autotag = pcall(require, 'nvim-ts-autotag')
-if (not status) then
-  return
+local status, autotag = pcall(require, "nvim-ts-autotag")
+if not status then
+	return
 end
 
-autotag.setup {}
+autotag.setup({})
