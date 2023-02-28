0df4c957a1f5099c94ed013e2965fe840468e67f
Dartsac <idobson.gatech@gmail.com>
2023-02-28 14:15:40 -0500

    edit autopairs to my liking

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -4,6 +4,13 @@ if not status_ok then
 	return
 end
 
+local rule_ok, Rule = pcall(require, "nvim-autopairs.rule")
+if not rule_ok then
+	return
+end
+
+local cond = require("nvim-autopairs.conds")
+
 npairs.setup({
 	check_ts = true,
 	ts_config = {
@@ -15,7 +22,7 @@ npairs.setup({
 	fast_wrap = {
 		map = "<C-e>",
 		chars = { "{", "[", "(", '"', "'" },
-		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,%;] ]], "%s+", ""),
 		offset = 0, -- Offset from pattern match
 		end_key = "$",
 		keys = "qwertyuiopzxcvbnmasdfghjkl",
@@ -24,6 +31,63 @@ npairs.setup({
 		highlight_grey = "LineNr",
 	},
 })
+
+npairs.add_rules({
+	Rule("%(.*%)%s*%=$", "> {}", { "typescript", "typescriptreact", "javascript" })
+		:use_regex(true)
+		:set_end_pair_length(1),
+	Rule("=", "")
+		:with_pair(cond.not_inside_quote())
+		:with_pair(function(opts)
+			local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+			if last_char:match("[%w%=%s]") then
+				return true
+			end
+			return false
+		end)
+		:replace_endpair(function(opts)
+			local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+			local next_char = opts.line:sub(opts.col, opts.col)
+			next_char = next_char == " " and "" or " "
+			if prev_2char:match("%w$") then
+				return "<bs> =" .. next_char
+			end
+			if prev_2char:match("%=$") then
+				return next_char
+			end
+			if prev_2char:match("=") then
+				return "<bs><bs>=" .. next_char
+			end
+			return ""
+		end)
+		:set_end_pair_length(0)
+		:with_move(cond.none())
+		:with_del(cond.none()),
+})
+
+local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+npairs.add_rules({
+	Rule(" ", " "):with_pair(function(opts)
+		local pair = opts.line:sub(opts.col - 1, opts.col)
+		return vim.tbl_contains({
+			brackets[1][1] .. brackets[1][2],
+			brackets[2][1] .. brackets[2][2],
+			brackets[3][1] .. brackets[3][2],
+		}, pair)
+	end),
+})
+for _, bracket in pairs(brackets) do
+	npairs.add_rules({
+		Rule(bracket[1] .. " ", " " .. bracket[2])
+			:with_pair(function()
+				return false
+			end)
+			:with_move(function(opts)
+				return opts.prev_char:match(".%" .. bracket[2]) ~= nil
+			end)
+			:use_key(bracket[2]),
+	})
+end
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")
