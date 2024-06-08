46bad856c952f46f8ce4612cbd72a92aa611b2c4
Dartsac <idobson.gatech@gmail.com>
2024-06-07 20:59:47 -0400

    lazygit whichkey update

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -104,28 +104,28 @@ local mappings = {
 		u = { "<cmd>PackerUpdate<cr>", "Update" },
 	},
 
-	g = {
-		name = "Git",
-		g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
-		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-		u = {
-			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-			"Undo Stage Hunk",
-		},
-		o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-		c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-		d = {
-			"<cmd>Gitsigns diffthis HEAD<cr>",
-			"Diff",
-		},
-	},
+	-- g = {
+	-- 	name = "Git",
+	-- 	g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+	-- 	j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+	-- 	k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+	-- 	l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+	-- 	p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+	-- 	r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+	-- 	R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+	-- 	s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+	-- 	u = {
+	-- 		"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+	-- 		"Undo Stage Hunk",
+	-- 	},
+	-- 	o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+	-- 	b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+	-- 	c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+	-- 	d = {
+	-- 		"<cmd>Gitsigns diffthis HEAD<cr>",
+	-- 		"Diff",
+	-- 	},
+	-- },
 
 	l = {
 		name = "LSP",
@@ -157,6 +157,7 @@ local mappings = {
 			"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
 			"Workspace Symbols",
 		},
+		g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
 	},
 	s = {
 		name = "Search",
