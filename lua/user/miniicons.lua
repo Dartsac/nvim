661d4f017e206c9b07a58b0dc9b4f78aaac02bf7
Dartsac <idobson.gatech@gmail.com>
2024-11-25 13:35:53 -0500

    match the Lua file icon and hl to init.lua

--- lua/user/miniicons.lua
+++ lua/user/miniicons.lua
@@ -1,4 +1,13 @@
 local miniicons = require("mini.icons")
+local icon, hl = miniicons.get("file", "file.lua")
 
-miniicons.setup()
+miniicons.setup({
+	style = "glyph", -- Use glyph icons
+	file = {
+		-- Explicitly set the icon for 'init.lua'
+		-- I like having init.lua with the same icon and hl as any other .lua file
+		["init.lua"] = { glyph = icon, hl = hl }, -- Match the Lua file icon
+	},
+})
+
 miniicons.mock_nvim_web_devicons()
