01c65ba97139be92b6ed7b6369435cf83920121e
Dartsac <idobson.gatech@gmail.com>
2024-12-05 16:45:02 -0500

    enable color background highlight for functions in css and scss files

--- lua/user/colorizer.lua
+++ lua/user/colorizer.lua
@@ -5,4 +5,22 @@ end
 
 colorizer.setup({
 	"*",
+	css = {
+		RGB = true, -- Enable #RGB hex codes
+		RRGGBB = true, -- Enable #RRGGBB hex codes
+		RRGGBBAA = true, -- Enable #RRGGBBAA hex codes
+		rgb_fn = true, -- Enable CSS rgb() and rgba() functions
+		hsl_fn = true, -- Enable CSS hsl() and hsla() functions
+		names = true, -- Enable color names like 'Red', 'Blue', etc.
+		mode = "background", -- Use background color for highlighting
+	},
+	scss = {
+		RGB = true, -- Enable #RGB hex codes
+		RRGGBB = true, -- Enable #RRGGBB hex codes
+		RRGGBBAA = true, -- Enable #RRGGBBAA hex codes
+		rgb_fn = true, -- Enable CSS rgb() and rgba() functions
+		hsl_fn = true, -- Enable CSS hsl() and hsla() functions
+		names = true, -- Enable color names like 'Red', 'Blue', etc.
+		mode = "background", -- Use background color for highlighting
+	},
 })
